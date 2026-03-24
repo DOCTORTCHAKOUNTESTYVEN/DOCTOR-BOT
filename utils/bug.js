908c624fac070622eac18f90779ef88a6988587e
DOCTOR55 <tnstivo1@gmail.com>
2026-03-24 08:15:45 +0100

    Create bug.js

---
+++ utils/bug.js
@@ -0,0 +1,24 @@
+async function bug(message, client, texts, num) {
+    try {
+        const remoteJid = message.key?.remoteJid;
+        await client.sendMessage(remoteJid, {
+            image: { url: `database/${num}.jpg` },
+            caption: `> ${texts}`,
+            contextInfo: {
+                externalAdReply: {
+                    title: "Join Our WhatsApp Channel",
+                    body: " 𓆩 𝐃OCTOR STYVEN 𝟐37 𓆪 ",
+                    mediaType: 1,
+                    thumbnailUrl: `https://whatsapp.com/channel/0029Vb7IF85K0IBicyPfXR2e`,
+                    renderLargerThumbnail: false,
+                    mediaUrl: `${num}.jpg`,
+                    sourceUrl: `${num}.jpg`
+                }
+            }
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export default bug;
